c3d1d054abb1b9b15273ccc4924e753f0d5f040e
dhkang <kangoona@naver.com>
2021-08-16 19:43:06 +0900

    2021.08.16 - by dhkang  - add fadein.xml fadeout.xml  - add Intent-Action Thread

--- app/src/main/java/com/sample/alc/ActivityIntro.java
+++ app/src/main/java/com/sample/alc/ActivityIntro.java
@@ -1,7 +1,9 @@
 package com.sample.alc;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class ActivityIntro extends Activity {
     private static final String TAG = ActivityIntro.class.getSimpleName();
@@ -10,9 +12,17 @@ public class ActivityIntro extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        Handler timer = new Handler();
+        timer.postDelayed(new Runnable(){ //2초후 쓰레드를 생성하는 postDelayed 메소드
 
-
-
+            public void run(){
+                //intent 생성
+                Intent intent = new Intent(ActivityIntro.this, MainActivity.class);
+                startActivity(intent); //다음 액티비티 이동
+                overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+                finish(); // 이 액티비티를 종료
+            }
+        }, 2000); //2000은 2초를 의미한다.
     }
 
     @Override
@@ -32,4 +42,5 @@ public class ActivityIntro extends Activity {
         super.onDestroy();
 
     }
+
 }
